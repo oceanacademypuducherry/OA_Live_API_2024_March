cfe098f218ea1ddaa10bc5fef81790894da1be3b
“ijassm” <ijass.ocean@gmail.com>
2024-03-23 17:29:14 +0530

    fix! : updated mongodb poolsize

--- DatabaseConnection.js
+++ DatabaseConnection.js
@@ -5,7 +5,7 @@ const mongoConnect = () => {
   // mongoose.connect(process.env.OFLINE_DATABASE_CONNECTION);
 
   mongoose.connect(
-    "mongodb+srv://root:ocean-academy-puducherry@oceanacademy.atrtb.mongodb.net/OA_Live_API?minPoolSize=400&maxPoolSize=500"
+    "mongodb+srv://root:ocean-academy-puducherry@oceanacademy.atrtb.mongodb.net/OA_Live_API?minPoolSize=0&maxPoolSize=100"
     // "mongodb+srv://root:root@oceanacademy.atrtb.mongodb.net/OA_Live_API"
   );
 };
